c85c506294290bfa43f0392f7a26c0c3ea5ef0fb
Simple <huyang1994822@gmail.com>
2025-04-24 23:31:38 +0800

    update arknights/gacha

--- arknights/gacha.go
+++ arknights/gacha.go
@@ -1,10 +1,45 @@
 package arknights
 
+import (
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+)
+
 type Gacha struct {
-	Ts   int64
-	Pool string
+    CharName string
+    GachaTs  string
+    IsNew    bool
+    PoolName string
+    Rarity   int
+}
+type User struct {
+    UID          string
+    AccountToken string
+    RoleToken    string
+    Cookie       string
 }
 
-func GachaPage(pageNo int, pageSize int) []Gacha {
-	return []Gacha{}
+func (u *User) GachaPage(pageNo int, pageSize int) []Gacha {
+    client := &http.Client{}
+
+    req, err := http.NewRequest("GET", fmt.Sprintf("https://ak.hypergryph.com/user/api/inquiry/gacha/history?uid=%s&category=classic&size=10", u.UID), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    req.Header.Set("x-account-token", u.AccountToken)
+    req.Header.Set("x-role-token", u.RoleToken)
+    req.Header.Set("cookie", u.Cookie)
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+    bodyText, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%s\n", bodyText)
+    return []Gacha{}
 }
